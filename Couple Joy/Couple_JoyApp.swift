5c669ad98b6644261c0b2ef95671bb96d67aae9e
Chinjan Patel <patel.chinjan531@gmail.com>
2025-05-06 20:14:57 +0530

    Initial Commit

---
+++ Couple Joy/Couple_JoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Couple_JoyApp.swift
+//  Couple Joy
+//
+//  Created by Chinjan Patel on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Couple_JoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Couple JoyTests/Couple_JoyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Couple_JoyTests.swift
+//  Couple JoyTests
+//
+//  Created by Chinjan Patel on 06/05/25.
+//
+
+import Testing
+@testable import Couple_Joy
+
+struct Couple_JoyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
