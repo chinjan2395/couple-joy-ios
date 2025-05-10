0fd7af1a389a3f5a800552bfa550ba571349e76a
Chinjan Patel <patel.chinjan531@gmail.com>
2025-05-10 21:37:30 +0530

    Updated parameter from text to message

--- Couple Joy/Models/Message.swift
+++ Couple Joy/Models/Message.swift
@@ -10,15 +10,15 @@ import FirebaseFirestore
 
 struct Message: Identifiable, Codable {
     @DocumentID var id: String?
-    var text: String
+    var message: String
     var timestamp: Date
     var uid: String
     var device: String
 //    var senderInitial: String
 
-    init(id: String? = nil, text: String, timestamp: Date, uid: String, device: String, senderInitial: String? = nil) {
+    init(id: String? = nil, message: String, timestamp: Date, uid: String, device: String, senderInitial: String? = nil) {
         self.id = id
-        self.text = text
+        self.message = message
         self.timestamp = timestamp
         self.uid = uid
         self.device = device
