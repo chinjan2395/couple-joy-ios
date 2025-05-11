80695d52d15eafef2d2cd2a02db558efde506f30
Chinjan Patel <patel.chinjan531@gmail.com>
2025-05-11 21:48:50 +0530

    Minor formatting corrections

--- Couple Joy/Services/FirestoreManager.swift
+++ Couple Joy/Services/FirestoreManager.swift
@@ -24,10 +24,16 @@ class FirestoreManager {
         completion: @escaping (Error?) -> Void
     ) {
         guard !coupleId.isEmpty else {
-                    print("Error: coupleId is empty in savePartnerInfo")
-                    completion(NSError(domain: "", code: 400, userInfo: [NSLocalizedDescriptionKey: "Invalid couple ID"]))
-                    return
-                }
+            print("Error: coupleId is empty in savePartnerInfo")
+            completion(
+                NSError(
+                    domain: "",
+                    code: 400,
+                    userInfo: [NSLocalizedDescriptionKey: "Invalid couple ID"]
+                )
+            )
+            return
+        }
         guard let uid = Auth.auth().currentUser?.uid else {
             completion(
                 NSError(
